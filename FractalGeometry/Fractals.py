57d36e868abcd77193f7e0d93a29010afb580279
rtmcardle <ryan.t.mcardle@gmail.com>
2021-01-28 11:33:41 -0500

    Reordered methods alphabetically to match images.

--- FractalGeometry/Fractals.py
+++ FractalGeometry/Fractals.py
@@ -50,6 +50,27 @@ class TurtleFractal():
         self.heading = t.heading()
         self.origin = t.pos()
 
+            
+    def saveImage(self,file_name):
+        """
+        Saves the current canvas as a .png image and resets the
+        Turtle for another drawing.
+
+        :param file_name: the name to save the file as
+        """
+
+        cur_dir = os.getcwd()
+        rec_dir = os.path.join(cur_dir,'Images\\')
+        if not os.path.exists(rec_dir):
+            os.mkdir(rec_dir)
+        ts=t.getcanvas()
+        psimage = ts.postscript(file= file_name+'.ps', colormode="color")
+        with Image.open(file_name+'.ps') as img:
+            img.save(os.path.join(rec_dir,file_name+'.png'), 'png', dpi=(1000,1000))
+        os.remove(file_name+'.ps')
+        self.setTurtle()
+
+
 
     def BarnsleyFern(self,dots=10**5):
         """
@@ -113,146 +134,6 @@ class TurtleFractal():
             self.BarnsleyLeaf(depth=depth-1,size=size/math.sqrt(2))
             t.left(45)
 
-
-    def HeighwayDragon(self, depth=12, size=200, parity=1,factor=1/math.sqrt(2)):
-        """
-        Draws the Heighway Dragon. This dragon can be 
-        approximated by drawing a line segment, then 
-        iteratively replacing line segments with the two 
-        short legs of a 45-45-90 triangle in alternating 
-        directions. This non-self-intersecting curve can be 
-        combined with copies of itself to tile the plane (see 
-        HeighwayPinwheel()).
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of the dragon
-        :param parity: tracks alternations in the recursive 
-            calls; changes direction of turns
-        :param factor: an overall scaling factor
-        """
-        if depth==0:
-            t.forward(size)
-        else:
-            t.left(parity*45)
-            self.HeighwayDragon(depth=depth-1, size=size*factor, parity=1)
-            t.right(parity*90)
-            self.HeighwayDragon (depth=depth-1, size=size*factor, parity=-1)
-            t.left(parity*45)
-
-
-    def HeighwayPinwheel(self,depth=6,size=200):
-        """
-        Draws four Heighway Dragons radiating from the origin.
-        These dragons will never intersect, and in the limit 
-        that depth -> inf and size -> inf, the curve fills the 
-        plane.
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of each dragon
-        """
-
-        num_arms = 4
-        for i in range(num_arms):
-            t.setposition(self.origin)
-            t.seth(self.heading+(360.0/num_arms)*i)
-            t.pendown()
-            self.HeighwayDragon(depth,size)
-            t.penup()
-
-
-    def SierpinskiDragon(self, depth=8, size=200, parity=1):
-        """
-        Draws the Sierpinski Dragon. Similar to the Heighway 
-        Dragon, can be approximated with a line segment, then
-        iteratively replacing line segments with one-half of a 
-        hexagon that would span the length of the segment, 
-        alternating directions. In the limit that depth -> inf,
-        the curve approximates the Sierpinski Gasket.
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of the dragon
-        :param parity: tracks alternations in the recursive 
-            calls; changes direction of turns
-        """
-
-        if depth == 0:
-            t.forward(size)
-        else:
-            t.left(60*parity)
-            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=-parity)
-            t.right(60*parity)
-            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=parity)
-            t.right(60*parity)
-            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=-parity)
-            t.left(60*parity)
-
-
-    def SierpinskiGasket(self,depth=9,size=200):
-        """
-        Rotates the Turtle so that SierpinskiDragon() draws a 
-        properly oriented Sierpinski Gasket.
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of the dragon
-        """
-
-        t.left(30)
-        t.penup()
-        t.setposition(size,-size*2/3)
-        t.pendown()
-        for _ in range(1):
-            self.SierpinskiDragon(depth=depth+1,size=800)
-            t.right(120)
-
-
-    def McWortersPentigree(self, depth=5,size=200,offangle=11.82, shrink=1/2.87):
-        """
-        Draws a variation on McWorter's Pentigree. Setting 
-        offangle = 36 and shrink = (3 + sqrt(5))/2 will 
-        generate the original pentigree. The default parameters
-        set here allow for the creation of the Pentadendrite.
-
-        :param depth: the number of recursive calls
-        :param size: the base size of the pentigree
-        :param offangle: an initial and final rotation angle 
-            for each recursive call
-        :param shrink: a scale factor between recursive calls
-        """
-
-        if depth==0:
-            t.forward(size)
-        else:
-            t.left(offangle)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.left(72)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.right(72)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.right(144)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.left(72)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.left(72)
-            self.McWortersPentigree(depth=depth-1, size=size*shrink)
-            t.right(offangle)
-
-
-    def Pentadendrite(self, depth=5,size=200):
-        """
-        Draws five copies of the defined variation of 
-        McWorter's Pentigree in a radial fashion to create the 
-        'Pentadendrite' pattern.
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of each pentigree.
-        """
-
-        t.penup()
-        t.setposition(size/(2*math.tan(math.pi/5)),-size/2)
-        t.pendown()
-        for _ in range(5):
-            self.McWortersPentigree(depth,size)
-            t.left(360//5)
 
 
     def EisensteinBoundary(self, depth=6, size=200, parity=1):
@@ -328,46 +209,6 @@ class TurtleFractal():
             t.left(120)
 
 
-    def KochCurve(self, depth=6, size=200):
-        """
-        Draws the Koch Curve. This curve is approximated by 
-        drawing a line, then replacing the middle third of the
-        line with two sides of an equilateral triangle, and 
-        iteratively repeating. This curve is used to create 
-        KochSnowFlake().
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of the boundary
-        """
-
-        if depth == 0:
-            t.forward(size)
-        else:
-            self.KochCurve(depth=depth-1, size=size/3)
-            t.left(60)
-            self.KochCurve(depth=depth-1, size=size/3)
-            t.right(120)
-            self.KochCurve(depth=depth-1, size=size/3)
-            t.left(60)
-            self.KochCurve(depth=depth-1, size=size/3)
-
-
-    def KochSnowFlake(self, depth=6, size=200):
-        """
-        Draws three copies of the Koch Curve in order to create
-        the boundary known as the Koch Snowflake. 
-
-        :param depth: the number of recursive calls to be made
-        :param size: the base size of the boundary
-        """
-
-        t.penup()
-        t.setposition(-math.sqrt(3)*size/4,-size/2)
-        t.pendown()
-        for _ in range(3):
-            self.KochCurve(depth,size)
-            t.right(120)
-
 
     def FibonacciSpiral(self):
         """
@@ -397,28 +238,195 @@ class TurtleFractal():
             a = b
             b = c
             c = a+b
+
+
+
+    def HeighwayPinwheel(self,depth=6,size=200):
+        """
+        Draws four Heighway Dragons radiating from the origin.
+        These dragons will never intersect, and in the limit 
+        that depth -> inf and size -> inf, the curve fills the 
+        plane.
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of each dragon
+        """
+
+        num_arms = 4
+        for i in range(num_arms):
+            t.setposition(self.origin)
+            t.seth(self.heading+(360.0/num_arms)*i)
+            t.pendown()
+            self.HeighwayDragon(depth,size)
+            t.penup()
+
+
+    def HeighwayDragon(self, depth=12, size=200, parity=1,factor=1/math.sqrt(2)):
+        """
+        Draws the Heighway Dragon. This dragon can be 
+        approximated by drawing a line segment, then 
+        iteratively replacing line segments with the two 
+        short legs of a 45-45-90 triangle in alternating 
+        directions. This non-self-intersecting curve can be 
+        combined with copies of itself to tile the plane (see 
+        HeighwayPinwheel()).
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of the dragon
+        :param parity: tracks alternations in the recursive 
+            calls; changes direction of turns
+        :param factor: an overall scaling factor
+        """
+        if depth==0:
+            t.forward(size)
+        else:
+            t.left(parity*45)
+            self.HeighwayDragon(depth=depth-1, size=size*factor, parity=1)
+            t.right(parity*90)
+            self.HeighwayDragon (depth=depth-1, size=size*factor, parity=-1)
+            t.left(parity*45)
+
+
+
+    def KochSnowFlake(self, depth=6, size=200):
+        """
+        Draws three copies of the Koch Curve in order to create
+        the boundary known as the Koch Snowflake. 
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of the boundary
+        """
+
+        t.penup()
+        t.setposition(-math.sqrt(3)*size/4,-size/2)
+        t.pendown()
+        for _ in range(3):
+            self.KochCurve(depth,size)
+            t.right(120)
+
+
+    def KochCurve(self, depth=6, size=200):
+        """
+        Draws the Koch Curve. This curve is approximated by 
+        drawing a line, then replacing the middle third of the
+        line with two sides of an equilateral triangle, and 
+        iteratively repeating. This curve is used to create 
+        KochSnowFlake().
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of the boundary
+        """
+
+        if depth == 0:
+            t.forward(size)
+        else:
+            self.KochCurve(depth=depth-1, size=size/3)
+            t.left(60)
+            self.KochCurve(depth=depth-1, size=size/3)
+            t.right(120)
+            self.KochCurve(depth=depth-1, size=size/3)
+            t.left(60)
+            self.KochCurve(depth=depth-1, size=size/3)
+
+
+
+    def Pentadendrite(self, depth=5,size=200):
+        """
+        Draws five copies of the defined variation of 
+        McWorter's Pentigree in a radial fashion to create the 
+        'Pentadendrite' pattern.
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of each pentigree.
+        """
+
+        t.penup()
+        t.setposition(size/(2*math.tan(math.pi/5)),-size/2)
+        t.pendown()
+        for _ in range(5):
+            self.McWortersPentigree(depth,size)
+            t.left(360//5)
+
+
+    def McWortersPentigree(self, depth=5,size=200,offangle=11.82, shrink=1/2.87):
+        """
+        Draws a variation on McWorter's Pentigree. Setting 
+        offangle = 36 and shrink = (3 + sqrt(5))/2 will 
+        generate the original pentigree. The default parameters
+        set here allow for the creation of the Pentadendrite.
+
+        :param depth: the number of recursive calls
+        :param size: the base size of the pentigree
+        :param offangle: an initial and final rotation angle 
+            for each recursive call
+        :param shrink: a scale factor between recursive calls
+        """
+
+        if depth==0:
+            t.forward(size)
+        else:
+            t.left(offangle)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.left(72)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.right(72)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.right(144)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.left(72)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.left(72)
+            self.McWortersPentigree(depth=depth-1, size=size*shrink)
+            t.right(offangle)
+
+
+
+    def SierpinskiGasket(self,depth=9,size=200):
+        """
+        Rotates the Turtle so that SierpinskiDragon() draws a 
+        properly oriented Sierpinski Gasket.
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of the dragon
+        """
+
+        t.left(30)
+        t.penup()
+        t.setposition(size,-size*2/3)
+        t.pendown()
+        for _ in range(1):
+            self.SierpinskiDragon(depth=depth+1,size=800)
+            t.right(120)
+
+
+    def SierpinskiDragon(self, depth=8, size=200, parity=1):
+        """
+        Draws the Sierpinski Dragon. Similar to the Heighway 
+        Dragon, can be approximated with a line segment, then
+        iteratively replacing line segments with one-half of a 
+        hexagon that would span the length of the segment, 
+        alternating directions. In the limit that depth -> inf,
+        the curve approximates the Sierpinski Gasket.
+
+        :param depth: the number of recursive calls to be made
+        :param size: the base size of the dragon
+        :param parity: tracks alternations in the recursive 
+            calls; changes direction of turns
+        """
+
+        if depth == 0:
+            t.forward(size)
+        else:
+            t.left(60*parity)
+            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=-parity)
+            t.right(60*parity)
+            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=parity)
+            t.right(60*parity)
+            self.SierpinskiDragon(depth=depth-1, size=size/2, parity=-parity)
+            t.left(60*parity)
+
+
           
-            
-    def saveImage(self,file_name):
-        """
-        Saves the current canvas as a .png image and resets the
-        Turtle for another drawing.
-
-        :param file_name: the name to save the file as
-        """
-
-        cur_dir = os.getcwd()
-        rec_dir = os.path.join(cur_dir,'Images\\')
-        if not os.path.exists(rec_dir):
-            os.mkdir(rec_dir)
-        ts=t.getcanvas()
-        psimage = ts.postscript(file= file_name+'.ps', colormode="color")
-        with Image.open(file_name+'.ps') as img:
-            img.save(os.path.join(rec_dir,file_name+'.png'), 'png', dpi=(1000,1000))
-        os.remove(file_name+'.ps')
-        self.setTurtle()
-
-
 
 def main():
     tf = TurtleFractal()
@@ -430,24 +438,24 @@ def main():
     #tf.saveImage('BarnsleyFern')
 
     #t.setpos(0,-275)
-    #tf.BarnsleyLeaf(depth=depths+2, size=sizes)
+    #tf.BarnsleyLeaf(depth=10, size=400)
     #tf.saveImage('BarnsleyLeaf')
 
-    #tf.HeighwayPinwheel(depth=depths+4, size=sizes)
-    #tf.saveImage('HeighwayPinwheel')
+    tf.HeighwayPinwheel(depth=12, size=sizes)
+    tf.saveImage('HeighwayPinwheel')
 
     #tf.SierpinskiDragon(depth=8, size=400)
 
-    #tf.SierpinskiGasket(depth=depths, size=sizes)
+    #tf.SierpinskiGasket(depth=8, size=400)
     #tf.saveImage('SierpinskiGasket')
 
-    #tf.Pentadendrite(depth=depths/2+2, size=sizes+100)
+    #tf.Pentadendrite(depth=6, size=500)
     #tf.saveImage('Pentadendrite')
 
-    tf.EisensteinBoundaryFull(depth=depths)
-    tf.saveImage('EisensteinBoundary1')
+    #tf.EisensteinBoundaryFull(depth=8)
+    #tf.saveImage('EisensteinBoundary')
 
-    #tf.KochSnowFlake(depth=6, size=400*1.5)
+    #tf.KochSnowFlake(depth=6, size=600)
     #tf.saveImage('KochSnowFlake')
 
     #tf.FibonacciSpiral()
